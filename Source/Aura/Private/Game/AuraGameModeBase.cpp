7bc62983d1aa830243f0098e1cbdc7415770c419
najongjine <54340913+najongjine@users.noreply.github.com>
2023-10-03 10:37:52 +0900

    .

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// CopyRight WildOjisan
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// CopyRight WildOjisan
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
